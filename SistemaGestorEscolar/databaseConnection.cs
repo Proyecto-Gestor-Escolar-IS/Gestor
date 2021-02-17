3d3c2d6fce401e3de8f3b38aa52640847bb125d7
Alexis <alexis.00363@outlook.com>
2021-02-16 21:25:20 -0600

    Commit de Prueba

--- SistemaGestorEscolar/databaseConnection.cs
+++ SistemaGestorEscolar/databaseConnection.cs
@@ -11,7 +11,7 @@ namespace SistemaGestorEscolar
     {
         /*Conexion a la base de datos*/
         //SqlConnection databaseIntermediary = new SqlConnection("server = 192.168.1.105,1433; database = StaMariaNazarethDatabaseService; User ID = mejiasoc; Password=paockyksyp1");
-        SqlConnection databaseIntermediary = new SqlConnection("server=DESKTOP-P4A3L4O; database = StaMariaNazarethDatabaseService; Integrated Security=True");
+        //SqlConnection databaseIntermediary = new SqlConnection("server=DESKTOP-P4A3L4O; database = StaMariaNazarethDatabaseService; Integrated Security=True");
 
         public void establecerConexion()
         {
