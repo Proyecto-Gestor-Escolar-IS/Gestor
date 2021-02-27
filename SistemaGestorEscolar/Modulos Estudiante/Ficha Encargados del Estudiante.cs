c219b98450a04427335bfba4f74f25d2fcf4a8a6
Andrea castro <saro2620@hotmail.com>
2021-02-26 23:06:10 -0600

    Modulos de Fichas del Estudiante y Modulo de Historial Medico

---
+++ SistemaGestorEscolar/Modulos Estudiante/Ficha Encargados del Estudiante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaGestorEscolar.Modulos_Estudiante
+{
+    public partial class Ficha_Encargados_del_Estudiante : Form
+    {
+        public Ficha_Encargados_del_Estudiante()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SistemaGestorEscolar/Modulos Estudiante/Ficha de Estudiante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaGestorEscolar.Modulos_Estudiante
+{
+    public partial class Ficha_de_Estudiante : Form
+    {
+        public Ficha_de_Estudiante()
+        {
+            InitializeComponent();
+        }
+
+        private void Ficha_de_Estudiante_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
